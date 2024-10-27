33c9038a0c05c46b857b3f77fea438418f80d0dd
shivghari <shiv.i.ghariwala@gmail.com>
2024-10-27 14:30:55 +0530

    feat: added the DB connection and data primting

--- posts/tests.py
+++ posts/tests.py
@@ -1,3 +1,26 @@
 from django.test import TestCase
+from .models import Post
 
-# Create your tests here.
+# This test is for the DB connectivity and the content validity
+class PostModalTest(TestCase):
+    def setUp(self):
+        Post.objects.create(text='Just a test text.')
+
+    def test_test_content(self):
+        post = Post.objects.get(id=1)
+        expected_object_name = f'{post.text}'
+        self.assertEqual(expected_object_name, 'Just a test text.')
+
+
+# This test is for the Web page rendering
+class PostPageViewTest(TestCase):
+    def setUp(self):
+        Post.objects.create(text="Just another test")
+
+    def test_view_url_exist_proper_location(self):
+        resp = self.client.get('/posts/')
+        self.assertEqual(resp.status_code, 200)
+
+
+    
+
--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Post
 
-# Create your views here.
+# Only this code can fetch all teh data from te DB and store in the variable name all_posts_list 
+class HomePageView(ListView):
+    model = Post
+    template_name = 'pages/posts.html'
+    context_object_name = 'all_posts_list' # this is the variable we ca use in HTML template 
